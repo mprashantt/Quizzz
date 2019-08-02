ca6755f0896cc82d3a5ab94d94077b28d4c18638
mprashantt <tprashant4648010@gmail.com>
2019-08-02 13:02:08 +0530

    connect UI items with code

--- Quizzz/ViewController.swift
+++ Quizzz/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newQuestion: UILabel!
+    @IBOutlet weak var questionNumber: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        
+    }
+    
 
 }
 
